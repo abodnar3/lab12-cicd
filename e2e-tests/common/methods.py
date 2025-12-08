597093537a9d5f78b1efa72191cc5923875130f2
Andrew Bodnar <abodnar3@users.noreply.githubg.com>
2025-12-08 03:28:27 +0000

    renamed test

--- e2e-tests/common/methods.py
+++ e2e-tests/common/methods.py
@@ -19,7 +19,7 @@ def anonymize(data):
     )
     return response.status_code, response.content
 
-def anonymize_genz(data):
+def genz(data):
     response = requests.post(
         f"{ANONYMIZER_BASE_URL}/genz",
         json=data, 
